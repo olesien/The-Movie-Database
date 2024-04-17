09b1020446ae5b0672f15550f5722a6ad39ac8a8
olesien <olesien2@gmail.com>
2024-04-17 17:01:10 +0200

    added start to genres

--- scripts/genres.js
+++ scripts/genres.js
@@ -1,6 +1,19 @@
 import { getGenres } from "./movieAPI.js";
+const genresEl = document.querySelector(".genres");
 const loadGenres = async () => {
     const genres = await getGenres();
+    genresEl.innerHTML = ""; //Clear DOM
+    genres.forEach((genre) => {
+        const li = document.createElement("li");
+
+        const a = document.createElement("a");
+        a.href = "/genre/" + genre.id;
+        a.innerText = genre.name;
+
+        li.appendChild(a);
+
+        genresEl.appendChild(li);
+    });
     console.log(genres);
 };
 
--- scripts/movieAPI.js
+++ scripts/movieAPI.js
@@ -2,14 +2,22 @@ const authKey =
     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmI1ZjllMzkxYTk4MGY0NmI0ZTZhMWJjNDYxMjUxNyIsInN1YiI6IjYyZmJjN2MyNmU5MzhhMDA3YTNjMTYxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.18qu5fJ5POzrxMgqTaXyHUeOOlEU_ORX9B1UNN74VNM";
 export const getGenres = async () => {
     //Return all genres
-    const res = await fetch(
-        "https://api.themoviedb.org/3/genre/movie/list?language=en",
-        {
-            headers: {
-                accept: "application/json",
-                Authorization: `Bearer ${authKey}`,
-            },
-        }
-    );
-    return res.json();
+    try {
+        const res = await fetch(
+            "https://api.themoviedb.org/3/genre/movie/list?language=en",
+            {
+                cache: "force-cache",
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer ${authKey}`,
+                },
+            }
+        );
+        const json = await res.json();
+        console.log(json);
+        return json?.genres ?? [];
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
 };
